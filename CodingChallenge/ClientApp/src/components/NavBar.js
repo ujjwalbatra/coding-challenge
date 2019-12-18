2e75e9e32ad6d51647e4a7618081a1770756c674
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-12-19 10:54:58 +1100

    front end vulnarabilities fixed

---
+++ CodingChallenge/ClientApp/src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import '../css/NavBar.css';
+
+export class NavBar extends Component {
+  static displayName = NavBar.name;
+
+  constructor (props) {
+    super(props);
+
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleNavbar () {
+    this.setState({
+      collapsed: !this.state.collapsed
+    });
+  }
+
+  render () {
+    return (
+      <header>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+          <Container>
+            <NavbarBrand tag={Link} to="/">CodingChallenge</NavbarBrand>
+          </Container>
+        </Navbar>
+      </header>
+    );
+  }
+}
