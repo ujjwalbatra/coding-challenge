ef463caf38a2e88eb36f02a0e4db585f2c4a650b
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-12-19 10:10:54 +1100

    initial commit + models added

---
+++ CodingChallenge/Models/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CodingChallenge.Models
+{
+    public class Car : Vehicle
+    {
+        private int NumOfDoors { get; set; }
+
+        private int NumOfWheels { get; set; }
+
+        private string BodyType { get; set; }
+
+        private string Engine { get; set; }
+
+
+        public Car(long id,
+                   string type,
+                   string make,
+                   string model,
+                   int numOfDoors,
+                   int numOfWheels,
+                   string bodyType,
+                   string engine) : base(id, type, make, model)
+        {
+            this.NumOfDoors = numOfDoors;
+            this.NumOfWheels = numOfWheels;
+            this.BodyType = bodyType;
+            this.Engine = engine;
+        }
+
+    }
+}
---
+++ CodingChallenge/Models/Vehicle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodingChallenge.Models
+{
+    public abstract class Vehicle
+    {
+        private long Id { get; set; }
+
+        private string Type { get; set; }
+
+        private string Make { get; set; }
+
+        private string Model { get; set; }
+
+        private int NumOfDoors { get; set; }
+
+        static List<Vehicle> vehicles = new List<Vehicle>();
+        //this data will ideally be kept in a db
+
+        protected Vehicle(long id, string type, string make, string model)
+        {
+            this.Id = id;
+            this.Type = type;
+            this.Make = make;
+            this.Model = model;
+        }
+
+        public static int AddVehicle(Vehicle vehicle)
+        {
+            try
+            {
+                int newVehicleId = vehicles.Count + 1;
+                vehicle.Id = newVehicleId;
+
+                vehicles.Add(vehicle);
+                return 1;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+
+        public static List<Vehicle> GetAllVehicles()
+        {
+            return vehicles;
+        }
+    }
+}
