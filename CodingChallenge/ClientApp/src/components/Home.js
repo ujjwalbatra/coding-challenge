9e05cb70f08e7eab28003f73714a72435b179d9b
Ujjwal Batra <ujjwalbatra2509@gmail.com>
2019-12-21 20:05:53 +1100

    routing issue fixed

--- CodingChallenge/ClientApp/src/components/Home.js
+++ CodingChallenge/ClientApp/src/components/Home.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
-import {Redirect} from 'react-router-dom';
 import '../css/home.css';
-import { AddCar } from './AddCar.js';
 import { Link } from 'react-router-dom'
 
 
 export class Home extends Component {
     static displayName = Home.name;
-
-    handleAddCar() {
-        return <Redirect to="/add-car" />;
-    }
 
     render () {
         return (
@@ -22,7 +16,7 @@ export class Home extends Component {
                     </Dropdown.Toggle>
 
                     <Dropdown.Menu>
-                        <Dropdown.Item component={Link} to='\add-car'>Car</Dropdown.Item>
+                        <Dropdown.Item><Link to="/add-car">Car</Link></Dropdown.Item>
                         <Dropdown.Item className='disabled'>Boat (coming soon...)</Dropdown.Item>
                         <Dropdown.Item className='disabled'>Bike (coming soon...)</Dropdown.Item>
                     </Dropdown.Menu>
